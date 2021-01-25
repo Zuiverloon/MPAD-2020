f825e3ad168f215ab0a6244474cad5f8b970a3af
Zuiverloon <50259991+Zuiverloon@users.noreply.github.com>
2021-01-25 14:31:49 +0800

    Create header.py

---
+++ WxpythonLab3/header.py
@@ -0,0 +1,62 @@
+#_*_coding:utf-8_*_
+
+
+#########################################
+#header
+#########################################
+
+
+Cmenu=[u"文件(&F)",
+       u"退出(&E)\tctrl+shift+q",
+       u"对话框(&D)",
+       u"模式对话框\tCtrl+x",
+       u"无模式对话框\tCtrl+y",
+       u"文件对话框\tCtrl+z",
+       u"光标(&C)",
+       u"光标&1\tCtrl+1",
+       u"光标&2\tCtrl+2",
+       u"光标&3\tCtrl+3",
+       u"语言(&L)",
+       u"中\tCtrl+C",
+       u"English\tCtrl+E",
+       u"关于(&A)",
+       u"程序信息\tCtrl+A",
+           
+      
+       ]
+
+Emenu=[u"&FILE",
+       u"&EXIT\tctrl+shift+q",
+       u"&DIALOG",
+       u"MODALDIALOG\tCtrl+x",
+       u"MODALLESSDIALOG\tCtrl+y",
+       u"FILEDIALOG\tCtrl+z",
+       u"&CURSOR",
+       u"CURSOR&1\tCtrl+1",
+       u"CURSOR&2\tCtrl+2",
+       u"CURSOR&3\tCtrl+3",
+       u"&LANGUAGE",
+       u"中\tCtrl+C",
+       u"English\tCtrl+E",
+       u"&ABOUT",
+       u"INFORMATION\tCtrl+A",
+       
+
+       ]
+
+CCText=[u"光标1",u"光标2",u"光标3"]
+ECText=["cursor1","cursor2","corsor3"]
+
+Cfile=u"所选文件名："
+Cft=u"文件"
+
+Efile="Filename："
+Eft="File"
+
+Cinfo=u"2020MPADLab6:Wx资源使用\n作者学号：10185102124 姓名：章君尧"
+Ctitle=u"2020MPADLab6:Wx资源使用"
+
+
+
+Einfo=u"2020MPADLab6:Wxresource\nnumber：10185102124 name：章君尧"
+Etitle=u"2020MPADLab6:Wxresource"
